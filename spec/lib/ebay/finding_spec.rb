a5414e9cd3abfc076a8540192daecc8111c76a88
Brad Reid <bradreid20@gmail.com>
2013-06-23 18:52:52 -0700

    add find by category spec

--- spec/lib/ebay/finding_spec.rb
+++ spec/lib/ebay/finding_spec.rb
@@ -5,6 +5,14 @@ describe Ebay::Finding do
     @ebay = Ebay::Finding.new
   end
 
+  context '#find_items_by_category' do
+    it 'should return a list of items' do
+      FakeWeb.register_uri(:post,  'http://svcs.ebay.com/services/search/FindingService/v1?', body: Ebay.root.join('spec/data/ebay_find_items_by_category.xml'))
+      result = @ebay.find_items_by_category
+      result.map(&:itemId).should == ["221244024846", "261229157049"]
+    end
+  end
+
   context '#get_item_status' do
     it 'should not blow up when the request fails' do
       FakeWeb.register_uri(:post,  'http://open.api.ebay.com/shopping', body: Ebay.root.join('spec/data/get_item_status_failed.xml'))
